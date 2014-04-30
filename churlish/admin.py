a5bfce642540b2a42652f7c9b13bf5f27ed2bfa3
Keryn Knight <keryn@kerynknight.com>
2014-04-30 19:06:00 +0100

    figured out a way to ask for relationship names at runtime, doesn’t distinguish between select_related/prefetch_related though yet.

--- churlish/admin.py
+++ churlish/admin.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from django.contrib import admin
 from .models import URL, URLRedirect, URLVisible
 
@@ -21,4 +22,23 @@ class URLAdmin(admin.ModelAdmin):
     search_fields = ['^path']
     ordering = ('-modified',)
     inlines = [VisibleInline, RedirectInline]
+
+    def get_runtime_relations(self):
+        """
+        For the middleware, of all things.
+        Because this is the best place to find out wtf we might want to
+        ask for at runtime without configuration.
+        """
+        models = (x.model for x in self.inlines[:] if hasattr(x, 'model'))
+        fields = (x._meta.get_fields_with_model() for x in models)
+        flat_fields = chain(*fields)
+        just_fields = (x[0] for x in flat_fields)
+        only_rels = (x for x in just_fields
+                     if hasattr(x, 'rel'))
+        only_good_rels = (x for x in only_rels if x.rel is not None)
+        only_ours = (x for x in only_good_rels if x.rel.to == URL)
+        accessors = (x.related.get_accessor_name() for x in only_ours
+                     if hasattr(x, 'related'))
+        return accessors
+
 admin.site.register(URL, URLAdmin)
